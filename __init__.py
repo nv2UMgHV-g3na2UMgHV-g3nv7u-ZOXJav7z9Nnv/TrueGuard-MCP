9dac4164104d69472034f9eb1336e6457703d399
nv2UMgHV-g3na2UMgHV-g3nv7u-ZOXJav7z9Nnv <michael.lei65@gmail.com>
2026-09-18 14:52:37 -0700

    Initialize __init__.py for scenario modules

---
+++ __init__.py
@@ -0,0 +1,7 @@
+"""Scenario modules for the demo runner."""
+
+from scenarios.scenario_a import scenario_a
+from scenarios.scenario_b import scenario_b
+from scenarios.scenario_c import scenario_c
+
+__all__ = ["scenario_a", "scenario_b", "scenario_c"]
